1648016bd6d0949413d2eed2f3468094008130cb
Ulrich Schmidt <u.schmidt@velian.de>
2017-04-21 16:12:02 +0200

    getCountryByCode

--- src/content/modules/country_list/helpers/CountryListHelper.php
+++ src/content/modules/country_list/helpers/CountryListHelper.php
@@ -5,7 +5,7 @@ class CountryListHelper extends Helper{
       if(!$language){
           $language = getCurrentLanguage(true);
       }
-      $file = getModulePath("country_list", true) . "/" .$folder. "/" . $language . ".txt";
+      $file = getModulePath("country_list", true) . "/data/" .$folder. "/" . $language . ".txt";
       // @FIXME: Prüfen ob Datei existiert
       // und Exception werfen wenn Datei nicht vorhanden
       $data = file_get_contents($data);
@@ -22,6 +22,33 @@ class CountryListHelper extends Helper{
           }
 
       }
-      return array();
+      return $result;
+  }
+  public function getCountryByCode($code, $language = null, $folder = "default"){
+      $result = null;
+      if(!$language){
+          $language = getCurrentLanguage(true);
+      }
+      $file = getModulePath("country_list", true) . "/data/" . $folder. "/" . $language . ".txt";
+      // @FIXME: Prüfen ob Datei existiert
+      // und Exception werfen wenn Datei nicht vorhanden
+      $data = file_get_contents($data);
+      $data = normalizeLN($data, "\n");
+      $data = explode("\n", $data);
+      foreach($data as $line){
+          $myLine = trim($line);
+          if(!empty($myLine)){
+              $splitted = explode(":", $myLine);
+              $country = new Country();
+              $country->code = trim($splitted[0]);
+              $country->name = trim($splitted[1]);
+              if($country->code == $code){
+                 $result = $country;
+                 break;
+              }
+          }
+
+      }
+      return $result;
   }
 }
