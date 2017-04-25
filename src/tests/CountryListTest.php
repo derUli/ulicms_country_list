95ade2b96aad1f7422c23445198bddb34cfea278
Ulrich Schmidt <u.schmidt@velian.de>
2017-04-25 10:03:24 +0200

    unit test für create update und delete

--- src/tests/CountryListTest.php
+++ src/tests/CountryListTest.php
@@ -1,5 +1,11 @@
 <?php
 class CountryListTest extends PHPUnit_Framework_TestCase {
+	public function setUp() {
+		Database::query ( "delete from {prefix}countries where countryName = 'Lampukistan'" );
+	}
+	public function tearDown() {
+		$this->setUp ();
+	}
 	public function testLoadByCountryCode() {
 		$country = new Country ();
 		$country->loadByCountryCode ( "KP" );
@@ -55,5 +61,66 @@ class CountryListTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals ( "ko-KP", $country->getLanguages () );
 		$this->assertEquals ( "PRK", $country->getIsoAlpha3 () );
 		$this->assertEquals ( 1873107, $country->getGeonameId () );
+	}
+	public function testCreateAndDelete() {
+		$country = new Country ();
+		$country->setCountryCode ( "LP" );
+		$country->setCountryName ( "Lampukistan" );
+		$country->setFipsCode ( "LMP" );
+		$country->setIsoNumeric ( "666" );
+		$country->setNorth ( 43.006054 );
+		$country->setSouth ( 37.673332 );
+		$country->setEast ( 130.674866 );
+		$country->setWest ( 124.315887 );
+		$country->setCapital ( "Porada Ninfu" );
+		$country->setContinentName ( "Asia" );
+		$country->setContinent ( "AS" );
+		$country->setLanguages ( "lp-LP" );
+		$country->setIsoAlpha3 ( "LMP" );
+		$country->setGeonameId ( 123456765 );
+		$country->save ();
+		$this->assertNotNull ( $country->getId () );
+		$this->assertEquals ( "LP", $country->getCountryCode () );
+		$this->assertEquals ( "Lampukistan", $country->getCountryName () );
+		$this->assertEquals ( "LMP", $country->getFipsCode () );
+		$this->assertEquals ( "666", $country->getIsoNumeric () );
+		$this->assertEquals ( 43.006054, $country->getNorth () );
+		$this->assertEquals ( 37.673332, $country->getSouth () );
+		$this->assertEquals ( 130.674866, $country->getEast () );
+		$this->assertEquals ( 124.315887, $country->getWest () );
+		$this->assertEquals ( "Porada Ninfu", $country->getCapital () );
+		$this->assertEquals ( "Asia", $country->getContinentName () );
+		$this->assertEquals ( "AS", $country->getContinent () );
+		$this->assertEquals ( "lp-LP", $country->getLanguages () );
+		$this->assertEquals ( "LMP", $country->getIsoAlpha3 () );
+		$this->assertEquals ( 123456765, $country->getGeonameId () );
+		
+		$id = $country->getId ();
+		
+		$country = new Country ( $id );
+		$this->assertNotNull ( $country->getId () );
+		$this->assertEquals ( "LP", $country->getCountryCode () );
+		$this->assertEquals ( "Lampukistan", $country->getCountryName () );
+		$this->assertEquals ( "LMP", $country->getFipsCode () );
+		$this->assertEquals ( "666", $country->getIsoNumeric () );
+		$this->assertEquals ( 43.006054, $country->getNorth () );
+		$this->assertEquals ( 37.673332, $country->getSouth () );
+		$this->assertEquals ( 130.674866, $country->getEast () );
+		$this->assertEquals ( 124.315887, $country->getWest () );
+		$this->assertEquals ( "Porada Ninfu", $country->getCapital () );
+		$this->assertEquals ( "Asia", $country->getContinentName () );
+		$this->assertEquals ( "AS", $country->getContinent () );
+		$this->assertEquals ( "lp-LP", $country->getLanguages () );
+		$this->assertEquals ( "LMP", $country->getIsoAlpha3 () );
+		$this->assertEquals ( 123456765, $country->getGeonameId () );
+		$country->setCapital ( "Lampaka" );
+		$this->assertEquals ( "Lampaka", $country->getCapital () );
+		$country->save ();
+		$country = new Country ( $id );
+		
+		$this->assertEquals ( "Lampaka", $country->getCapital () );
+		$country->delete ();
+		$country = new Country ( $id );
+		$this->assertNull ( $country->getId () );
 	}
 }
